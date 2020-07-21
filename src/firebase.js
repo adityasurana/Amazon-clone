03496eecdb6dc5cf7e53703063e5b15215b65f79
Aditya Surana <to.adityasurana@gmail.com>
2020-07-21 18:59:47 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase'
 
 const firebaseApp = firebase.initializeApp({
-        apiKey: "AIzaSyBzD-Gc0RdQgzyh8CRSYKLR_3dCXwEqpvU",
-        authDomain: "clone-c946e.firebaseapp.com",
-        databaseURL: "https://clone-c946e.firebaseio.com",
-        projectId: "clone-c946e",
-        storageBucket: "clone-c946e.appspot.com",
-        messagingSenderId: "845790834521",
-        appId: "1:845790834521:web:cb5401817653b45970a648",
-        measurementId: "G-G8H35PFF97"
+        apiKey: "#",
+        authDomain: "#",
+        databaseURL: "#",
+        projectId: "#",
+        storageBucket: "#",
+        messagingSenderId: "#",
+        appId: "#",
+        measurementId: "#"
 })
 
 
